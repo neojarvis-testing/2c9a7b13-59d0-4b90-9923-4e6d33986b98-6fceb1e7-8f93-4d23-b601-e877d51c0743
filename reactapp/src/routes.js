8e3c6376c2245fab6aa6e25dcfe186670296814b
ashokbhati178 <erbhati1990@gmail.com>
2025-05-25 07:14:09 +0000

    Signup page html css validations

--- reactapp/src/routes.js
+++ reactapp/src/routes.js
@@ -1,11 +1,13 @@
 import { createRoutesFromElements, Navigate, Route, useLocation } from "react-router-dom";
 import Login from "./Components/Login";
 import ErrorPage from "./Components/ErrorPage";
+import Signup from "./Components/Signup";
 const NotFound = () => <div>404</div>;
 
 export default createRoutesFromElements(
     <Route>
         <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
         <Route path="/error" element={<ErrorPage/>} />
         <Route element={<NotFound/>} />
     </Route>
